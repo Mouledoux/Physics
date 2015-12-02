c43c285b12e8af933dd606d42091a59bfb06603c
Eric <EricMouledoux@Gmail.com>
2015-12-01 23:21:42 -0600

    "Spring" physics does... something
    But current only with unity rigidbidy.
    Else its more like a leash than a spring.

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        float dist = Vector3.Distance(transform.position, anchor);
+        transform.position += (anchor - transform.position) * Time.deltaTime * dist * spring;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+	}
+
+    public float spring;
+    public Vector3 anchor;
+}
