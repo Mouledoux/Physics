c04f3f9447cecdc1f853b439f5b861fbcbc66845
Eric <EricMouledoux@Gmail.com>
2015-12-01 22:46:55 -0600

    ?

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour
+{
+	void Update ()
+    {
+
+	}
+}
---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour
+{
+    void Start()
+    {
+        if(obj1 && obj2)
+        {
+            m_size = Vector3.Distance(obj1.transform.position, obj2.transform.position);
+        }
+    }
+
+
+	void Update ()
+    {
+	    
+	}
+
+    public GameObject obj1;
+    public GameObject obj2;
+
+    public float springStrength;
+
+    private float m_size;
+}
